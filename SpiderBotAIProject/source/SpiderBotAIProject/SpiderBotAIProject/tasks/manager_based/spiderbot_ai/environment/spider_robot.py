8cad236fb0b2631c33c40035c3578d079baa4f31
saneaven <saneaven@gmail.com>
2026-02-27 21:34:57 -0800

    More physics iteration

--- SpiderBotAIProject/source/SpiderBotAIProject/SpiderBotAIProject/tasks/manager_based/spiderbot_ai/environment/spider_robot.py
+++ SpiderBotAIProject/source/SpiderBotAIProject/SpiderBotAIProject/tasks/manager_based/spiderbot_ai/environment/spider_robot.py
@@ -89,8 +89,8 @@ SPIDER_CFG = ArticulationCfg(
         ),
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
             enabled_self_collisions=True,
-            solver_position_iteration_count=4,
-            solver_velocity_iteration_count=0,
+            solver_position_iteration_count=8,
+            solver_velocity_iteration_count=4,
         ),
         collision_props=sim_utils.CollisionPropertiesCfg(
             contact_offset=0.01,
